18f3aa9456824848fd14413c8ca74ea354083cde
luca4s <96322105+luca4s@users.noreply.github.com>
2023-08-11 23:42:26 -0300

    commit (5)

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const title = document.getElementById("title");
-const text = document.getElementById("text");
 $.get("https://lucsbot-2a46d8c3d1b2.herokuapp.com/" + window.location.search, function(data) {
+    console.log(data);
     window.location.href = data;
 });
--- redirect.js
+++ redirect.js
@@ -1,6 +1,5 @@
-const title = document.getElementById("title");
-const text = document.getElementById("text");
 $.get("https://lucsbot-2a46d8c3d1b2.herokuapp.com/redirect" + window.location.search, function(data) {
-    title.innerHTML = data.title;
-    text.innerHTML = data.text;
+    console.log(data);
+    document.getElementById("title").innerHTML = data.title;
+    document.getElementById("text").innerHTML = data.text;
 });
