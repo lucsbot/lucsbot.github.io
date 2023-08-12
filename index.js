d3022801b5457ed1181d214ab69b021a3185b7a8
luca4s <96322105+luca4s@users.noreply.github.com>
2023-08-12 14:44:47 -0300

    commit (6)

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 $.get("https://lucsbot-2a46d8c3d1b2.herokuapp.com/" + window.location.search, function(data) {
     console.log(data);
-    window.location.href = data;
+    if (data.url) {
+        window.location.href = data.url;
+    } else {
+        document.getElementById("title").innerHTML = data.title;
+        document.getElementById("text").innerHTML = data.text;
+    }
 });
